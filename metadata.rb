99d1d387178f9058f99226fe0577699e441c3bf8
Kirill Shevchenko <kirillweb@users.noreply.github.com>
2016-11-09 14:12:04 +0200

    0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'kirills167@gmail.com'
 license          'All rights reserved'
 description      'Install libssl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
